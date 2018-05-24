954d4139ec9ec72f6a7a82f137edb3fe48910a72
Alejandra Castillo <ahle.castillo@gmail.com>
2018-05-24 19:55:23 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -22,6 +22,8 @@ function sayHiToGrandma(string) {
   if (string.toUpperCase() === string) 
     return "Yes INDEED"
   }
-  if (string === "I love you, Grandma.") { 
+  if (string === "I love you, Grandma.")
     return "I love you, too."
+  } 
+  
 
